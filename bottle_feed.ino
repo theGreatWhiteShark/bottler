4bc1bd6cd6fea3dd3cc3001097b74480fdae04b7
theGreatWhiteShark <thetruephil@googlemail.com>
2020-08-19 11:00:22 +0200

    start rewriting in objective style

--- bottle_feed.ino
+++ bottle_feed.ino
@@ -1,129 +1,159 @@
 //Sample using LiquidCrystal library
 #include <LiquidCrystal.h>
 
-/*******************************************************
- * 
- * This program will test the LCD panel and the buttons
- * Mark Bramwell, July 2010
- * 
- ********************************************************/
-
-
-// select the pins used on the LCD panel
-LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
-
-// define some values used by the panel and buttons
-int lcd_key     = 0;
-int adc_key_in  = 0;
-#define btnRIGHT  0
-#define btnUP     1
-#define btnDOWN   2
-#define btnLEFT   3
-#define btnSELECT 4
-#define btnNONE   5
-
-// Values respresenting the different views.
-// 0 - Bottle history
-// 1 - Menu
-// 2 - Enter a new bottle
-// 3 - Total consumption within the last 24 hours
-// 4 - Set time
-int current_view = 0;
-#define VIEW_HISTORY            0
-#define VIEW_MENU               1
-#define VIEW_NEW_BOTTLE         2
-#define VIEW_TOTAL_CONSUMPTION  3
-#define VIEW_SET_TIME           4
-
 /* Amount of microseconds the program will pause after detecting a button event. This is necessary since several button events per second would be detected otherwise.*/
 #define DELAY_TIME 100
-
 #define DEFAULT_VOLUME 140
 #define DEFAULT_SPOONS 3.5
-
 #define MAX_BOTTLES 256
+
+enum Button { right, up, down, left, select, none };
+
+enum Views { history, menu, newBottle, consumption, time };
+
+class Interface {
+
+public:
+
+	void view();
+
+private:
+
+	void bottleView();
+	void historyView();
+	void menuView();
+	void newBottleView();
+	void consumptionView();
+	void setTimeView();
+
+	int readButtons() static;
+	// Resets the global variables to their initial state (done before entering the view).
+	void restoreDefaults();
+	
+	// Specify the pins used by the LCD
+	LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+
+	Crate crate;
+
+	int lcd_key      = 0;
+	int adc_key_in   = 0;
+	int current_view = 0;
+	
+	// Specifies which bottle in bottles is the most recent one.
+	unsigned current_bottle = 0;
+	unsigned displayed_bottle = 0;
+	
+	// Global constants used within the functions.
+
+	// Additional constant for browsing the different menu options.
+	// 2 - new bottle
+	// 3 - total consumption
+	// 4 - set time
+	int menu_item=2;
+	int old_menu_item=-1;
+
+	// One after another the
+	// 0 - amount of spoons
+	// 1 - volume
+	// 2 - time of creation will be inserted.
+	// The following variable will keep track of which is the current one.
+	int bottle_option = 0;
+	int old_bottle_option = -1;
+	float bottle_spoons = DEFAULT_SPOONS;
+	float old_bottle_spoons = -1.0;
+	int bottle_volume = DEFAULT_VOLUME;
+	int old_bottle_volume = -1;
+	int bottle_time = 12;
+	int old_bottle_time = -1;
+	int old_displayed_bottle = -1;
+
+};
+
 
 struct Bottle {
   float spoons;
-  byte volume;
-  byte remaining;
-  byte time;
+  int volume;
+  int remaining;
+  int time;
 };
 
-Bottle bottles[MAX_BOTTLES];
-// Specifies which bottle in bottles is the most recent one.
-unsigned current_bottle = 0;
-unsigned displayed_bottle = 0;
-
+class Crate {
+public:
 /* Adds a new bottle to the global bottles array and increments
    current_bottle. This array will be filled in a cyclic fashion while
    overwriting the oldest entry.
 
 Upon adding a bottle is considered full.*/
-void addBottle(float spoons, byte volume, byte time)
-{
-  Serial.println("current_bottle");
-  Serial.println(current_bottle);
+	void addBottle(float spoons, int volume, int time);
 
+	int totalVolume(int from, int till);
+	int totalSpoons(int from, int till);
+
+	byte get_current_bottle() static;
+	Bottle get_bottle(int index) static;
+	
+private:
+	Bottle bottles[MAX_BOTTLES];
+
+	byte current_bottle;
+};
+
+inline byte Crate::get_current_bottle() static {
+	return current_bottle;
+}
+inline Bottle Crate::get_bottle(int index) static {
+	return bottles[index];
+}
+
+
+
+void Crate::addBottle(float spoons, int volume, int time)
+{
   Bottle new_bottle = {spoons, volume, volume, time};
 
   current_bottle++;
-  if (current_bottle >= MAX_BOTTLES) {
+  if ( current_bottle >= MAX_BOTTLES ) {
     current_bottle = 0;
   }
 
   bottles[current_bottle] = new_bottle;
 }
 
-// read the buttons
-int readLCDButtons()
+int Interface::readButtons() static {
 {
   adc_key_in = analogRead(0);      // read the value from the sensor
 
   // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
   // we add approx 50 to those values and check to see if we are close
-  if (adc_key_in > 790) return btnNONE; // We make this the 1st option for speed reasons since it will be the most likely result
+  if ( adc_key_in > 790 ) {
+	  return Button::none;
+  }
+
+  // We make this the 1st option for speed reasons since it will be the most likely result
 
   delay(DELAY_TIME);
 
-  if (adc_key_in < 50)   return btnRIGHT;
-  if (adc_key_in < 195)  return btnUP;
-  if (adc_key_in < 380)  return btnDOWN;
-  if (adc_key_in < 555)  return btnLEFT;
-  if (adc_key_in < 790)  return btnSELECT;
+  if ( adc_key_in < 50 ) {
+	  return Button::right;
+  }
+  if ( adc_key_in < 195 ) {
+	  return Button::up;
+  }
+  if ( adc_key_in < 380 ) {
+	  return Button::down;
+  }
+  if ( adc_key_in < 555 ) {
+	  return Button::left;
+  }
+  if ( adc_key_in < 790 ) {
+	  return Button::select;
+  }
 
-  return btnNONE;  // when all others fail, return this...
+  return Button::none;
 }
 
-// Time will be handled in 
-
-
-// Global constants used within the functions.
-
-// Additional constant for browsing the different menu options.
-// 2 - new bottle
-// 3 - total consumption
-// 4 - set time
-int menu_item=2;
-int old_menu_item=-1;
-
-// One after another the
-// 0 - amount of spoons
-// 1 - volume
-// 2 - time of creation will be inserted.
-// The following variable will keep track of which is the current one.
-int bottle_option = 0;
-int old_bottle_option = -1;
-float bottle_spoons = DEFAULT_SPOONS;
-float old_bottle_spoons = -1.0;
-int bottle_volume = DEFAULT_VOLUME;
-int old_bottle_volume = -1;
-int bottle_time = 12;
-int old_bottle_time = -1;
-int old_displayed_bottle = -1;
-
 // Resets the global variables to their initial state (done before entering the view).
-void restoreDefaults()
+ void Interface::restoreDefaults()
 {
   menu_item = 2;
   old_menu_item = -1;
@@ -141,13 +171,6 @@ void restoreDefaults()
   old_displayed_bottle = -1;
 }
 
-void setup()
-{
-  lcd.begin(16, 2);
-  lcd.setCursor(0,0);
-  lcd.print("Bottle-Spass");
-  Serial.begin(9600);
-}
 
 /* General view displaying a single bottle (determined by `index`
    within the array `bottles`. */
@@ -623,10 +646,18 @@ void startView(byte selection)
   }
 }
 
+void setup()
+{
+  lcd.begin(16, 2);
+  lcd.setCursor(0,0);
+  lcd.print("Bottle-Spass");
+  Serial.begin(9600);
+}
+
 void loop()
 {
   // Update the LCD display.
-  startView(current_view);
+  view(current_view);
 }
 
 
