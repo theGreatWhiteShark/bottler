9ebf54824f1a161511f93aa12e723876074eb4f3
theGreatWhiteShark <thetruephil@googlemail.com>
2020-09-12 09:24:02 +0200

    add license to source file

--- bottler.ino
+++ bottler.ino
@@ -1,4 +1,18 @@
-//Sample using LiquidCrystal library
+// Copyright (C) 2020  Philipp Müller
+
+// bottler is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// bottler is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with bottler.  If not, see <https://www.gnu.org/licenses/>.
+
 #include <RTClib.h>
 #include <LiquidCrystal.h>
 
